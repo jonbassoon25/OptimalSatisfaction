6de497d05eb55cd4d77e5c9c7c6f318ef3fbeef1
Jonathan Hanson <jonbassoon25@gmail.com>
2025-01-29 15:12:38 -0700

    Added get branch function in production tree and fixed numeric path example

--- Production_Tree_Dataclasses.py
+++ Production_Tree_Dataclasses.py
@@ -12,27 +12,45 @@ class production_tree:
 		'''
 		self.production_tree = production_tree_dict
 	
-	def get_branch(self, numeric_identifier):
-		pass
+	def get_branch(self, numeric_path, production_tree = None):
+		if production_tree == None:
+			production_tree = self.production_tree
+
+		branch = {}
+		current_key = list(production_tree.keys())[numeric_path[1]]
+		branch[current_key] = []
+		
+		numeric_index = 3
+		input_index = 0
+		while numeric_index < len(numeric_path):
+			
+			branch[current_key].append(self.get_branch(numeric_path[numeric_index:numeric_index+numeric_path[numeric_index]], production_tree[current_key][input_index]))
+			numeric_index += numeric_path[numeric_index]
+			input_index += 1
+
+		
+		return branch
+		
+
 
 class simple_path:
-	def __init__(self, path_infornmation, iterable_identifier):
+	def __init__(self, path_information, iterable_path):
 		'''
-		Creates a simple production path with an numberic identifier so it's detailed path may be located within the larger production tree
+		Creates a simple production path with an iterable_path so that it's detailed path may be determined using the larger production tree
 
 		Parameters:
-			path_infornmation (dict): dictionary containing basic path information (final recipe name, inputs, outputs, construction costs, energy use)
-			iterable_identifier (iterable): ordered iterable of numbers representing the decisions to get this path in the production tree
+			path_information (dict): dictionary containing basic path information (final recipe name, inputs, outputs, construction costs, energy use)
+			iterable_path (iterable): ordered iterable of numbers representing the decisions to get this path in the production tree
 		'''
-		self.details = path_infornmation
+		self.details = path_information
 
-		self.numeric_path = np.array(iterable_identifier, dtype="uint8")
+		self.numeric_path = np.array(iterable_path, dtype="uint8")
 		'''
 		numeric path formatting:
 		[
 			section length,
 			first decision, 
-			1 input, 
+			2 inputs,
 			section length,
 			second decision,
 			2 inputs, 
@@ -45,5 +63,5 @@ class simple_path:
 			section length,
 			first decision,
 			0 inputs
-		] -> [15, 0, 1, 12, 1, 2, 3, 0, 0, 6, 2, 1, 3, 0, 0]
+		] -> [21, 0, 2, 12, 1, 2, 3, 0, 0, 6, 2, 1, 3, 0, 0, 6, 1, 1, 3, 0, 0]
 		'''
