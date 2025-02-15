afead7f7ea81cd5b8ed89a35522319c8390d55fa
Jonathan Hanson <jonbassoon25@gmail.com>
2025-02-14 21:33:11 -0700

    Add comments to User_Interface

--- User_Interface.py
+++ User_Interface.py
@@ -8,6 +8,7 @@ import Setup_Generator
 
 class Optimal_Satisfaction_UI:
 	def __init__(self):
+		'''Generates the main tkinter UI that is started by a call to this object's mainloop method'''
 		self.root = Tk()
 		
 		root = self.root
@@ -65,13 +66,9 @@ class Optimal_Satisfaction_UI:
 		#construction_limit_display = Display_Box(mainframe, root, text="Construction Limits", content=self.construction_limits, allow_zero=True)
 		#construction_limit_display.grid(column=7, row=1, columnspan=2, padx=5, sticky="NW")
 
-		
 		calculate = Button(mainframe, text="Calculate Paths", command=lambda:Production_Paths_Window(self, root, self._generate_production_tree()))
 		calculate.grid(column=0, row=10, columnspan=2)
 
-
-		#ds = Drag_Sort(root, ["Hello", "World", "test", "test", "test"])
-		#ds.grid(column=0, row=5)
 
 	def _generate_production_tree(self):
 		if int(self.production_rate_str.get()) == 0 or self.output_str.get() == "":
@@ -87,17 +84,12 @@ class Optimal_Satisfaction_UI:
 			raise Exception(f"Unrecognized miner tier from self.miner_tier: {self.miner_tier.get}")
 		return Setup_Generator.generate_production_tree(self.output_str.get(), int(self.production_rate_str.get()), miner_tier, default_only=self.recipe_type.get() == "default", input_resources=Util.display_box_content_to_dict(self.input_resources))
 	
-
 	def mainloop(self):
+		'''Calls the tkinter mainloop function for this object's UI'''
 		self.root.mainloop()
 
-
-	def calculate_production_rate(self):
-		'''Calculates production rate from inputs and updates production rate input box to match the calculated value'''
-		print("Calc production rate")
-	
-
 	def validate_production_rate_input(self):
+		'''Validates production rate input box by only allowing integers as inputs'''
 		pri = list(self.production_rate_str.get())
 		for char in pri:
 			try:
@@ -108,6 +100,7 @@ class Optimal_Satisfaction_UI:
 		self.production_rate_str.set(''.join(pri))
 	
 	def finalize_production_rate_input(self):
+		'''Does final validations to the production rate input to make sure it can be converted to an integer value'''
 		self.validate_production_rate_input()
 		if self.production_rate_str.get() == "":
 			self.production_rate_str.set("0")
@@ -117,6 +110,13 @@ class Optimal_Satisfaction_UI:
 
 class Item_Selection_Window:
 	def __init__(self, root, display_box):
+		'''
+		Generates a window for item selection with a parent window and linked display box
+		
+		Parameters:
+			(tkinter_window) root: master window of this window
+			(Display_Box) display_box: display box to link to this window
+		'''
 		self.display_box = display_box
 
 		self.root = Toplevel(root)
@@ -146,6 +146,7 @@ class Item_Selection_Window:
 
 
 	def validate_production_rate_input(self):
+		'''Validates production rate input box by only allowing integers as inputs'''
 		pri = list(self.production_rate_str.get())
 		for char in pri:
 			try:
@@ -156,6 +157,7 @@ class Item_Selection_Window:
 		self.production_rate_str.set(''.join(pri))
 	
 	def finalize_production_rate_input(self):
+		'''Does final validations to the production rate input to make sure it can be converted to an integer value'''
 		self.validate_production_rate_input()
 		if self.production_rate_str.get() == "":
 			self.production_rate_str.set("0")
@@ -163,7 +165,7 @@ class Item_Selection_Window:
 			self.production_rate_str.set(str(int(self.production_rate_str.get())))
 
 	def add(self):
-		#self.listvariable.set((self.listvariable.get() + "".join([" " + Items.get_item_by_name(self.item_list[i]).__name__ for i in self.item_listbox.curselection()])).strip())
+		'''Adds the selected item of this window's listbox to this window's linked display box with the specified production rate'''
 		if len(self.item_listbox.curselection()) == 0:
 			return
 		self.display_box.add_content([[self.item_list[self.item_listbox.curselection()[0]], int(self.production_rate_str.get())]])
@@ -171,10 +173,19 @@ class Item_Selection_Window:
 		self.destroy()
 	
 	def destroy(self):
+		'''Destroyes this window'''
 		self.root.destroy()
 	
+
 class Item_Deletion_Window:
 	def __init__(self, root, display_box):
+		'''
+		Generates a window for item deletion with a parent window and linked display box
+		
+		Parameters:
+			(tkinter_window) root: master window of this window
+			(Display_Box) display_box: display box to link to this window
+		'''
 		self.display_box = display_box
 
 		self.root = Toplevel(root)
@@ -192,7 +203,9 @@ class Item_Deletion_Window:
 		remove = Button(mainframe, text="Remove", command=self.remove)
 		remove.grid(column=0, row=1)
 
+
 	def remove(self):
+		'''Removes the selected item of this window's listbox from this window's linked display box'''
 		if len(self.selection_listbox.curselection()) == 0:
 			return
 		content = self.display_box.get_content()
@@ -201,12 +214,15 @@ class Item_Deletion_Window:
 		self.destroy()
 
 	def destroy(self):
+		'''Destroyes this window'''
 		self.root.destroy()
 
 
 class Display_Box(Frame):
 	def __init__(self, master, root, text="Display Box", content=[], allow_zero=False):
 		'''
+		Generates a display box that can be placed within the tkinter grid system
+
 		Parameters:
 			master: master
 			content (list): content. format: [["item name", quantity], ...]
@@ -230,10 +246,17 @@ class Display_Box(Frame):
 
 
 	def get_content(self):
+		'''Returns a shallow copy of this object's content'''
 		return self.content[:]
 	
 	def set_content(self, content):
-		content = content[:] #if content == self.content, it should be seperated on at least the surface level
+		'''
+		Sets this object's content
+		
+		Parameters:
+			(list) content: the new content to set this object's content to
+		'''
+		content = content[:] #if content == self.content it should be seperated, at least on the surface level
 		self.content.clear()
 		for label in self.labels:
 			label.destroy()
@@ -242,12 +265,19 @@ class Display_Box(Frame):
 		self.add_content(content)
 	
 	def add_content(self, content):
+		'''
+		Adds content to this object's content list
+
+		Parameters:
+			(list) content: the new content to add to this object's content list
+		'''
 		for thing in content:
 			self.labels.append(Label(self, text=f"{thing[0]}: {thing[1]} / min"))
 			self.labels[-1].grid(column=0, row=len(self.labels), padx=7, pady=1, sticky="W", columnspan=2)
 		self.content += content
 	
 	def validate_display_values(self):
+		'''Validates the numeric values of items in this display box'''
 		for thing in self.content:
 			if thing[1] == 0 and not self.allow_zero:
 				self.content.remove(thing)
@@ -256,6 +286,15 @@ class Display_Box(Frame):
 
 class Production_Paths_Window:
 	def __init__(self, main_window, root, production_tree):
+		'''
+		Generates a window displaying the possilble paths of the provided production tree
+		Additional information about each path can be accessed by double clicking it's line in this object's listbox
+
+		Parameters:
+			main_window (Optimal_Satisfaction_UI): UI Window with production tree generation values
+			root (tkinter_window): master window of this window
+			production_tree (dict): production tree to represent the paths of
+		'''
 		if production_tree == None:
 			return
 		
@@ -263,17 +302,13 @@ class Production_Paths_Window:
 		root = self.root
 
 		self.production_tree = production_tree
-		print("Saving Production Tree...")
-
-		with open("text.txt", "w") as dumpFile:
-			dumpFile.write(str(self.production_tree))
 
 		print("Splitting tree...")
 		self.production_paths = Setup_Generator.split_production_tree(production_tree)
 		print("Filtering tree...")
 		self.production_paths = Setup_Generator.filter_production_paths(self.production_paths, main_window.output_str.get(), int(main_window.production_rate_str.get()))
 		print("Sorting tree...")
-		self.production_paths = Setup_Generator.sort_production_paths(self.production_paths, main_window.output_str.get(), ["resource efficiency", "input resources", "byproducts", "electrical consumption", "construction cost"], {})
+		self.production_paths = Setup_Generator.sort_production_paths(self.production_paths, main_window.output_str.get(), ["resource efficiency", "input resources", "byproducts", "electrical consumption", "construction cost"], {}) #for input weights: {} -> Util.display_box_content_to_dict(main_window.input_resources)
 		print("Getting simple paths...")
 		self.simple_production_paths = Setup_Generator.get_simple_production_paths(self.production_paths)
 
@@ -296,6 +331,14 @@ class Production_Paths_Window:
 
 class Production_Path_Window:
 	def __init__(self, root, simple_production_path, production_path):
+		'''
+		Generates a window displaying detailed information about a specific production path
+
+		Parameters:
+			(tkinter_window) root: master window of this window
+			(dict) simple_production_path: the simplified production path of the provided production path
+			(dict) production_path: the production path to display the values of
+		'''
 		self.root = Toplevel(root)
 		root = self.root
 
@@ -328,7 +371,6 @@ class Production_Path_Window:
 			key = list(simple_production_path["inputs"].keys())[i]
 			Label(input_frame, text=f"{key}: {round(simple_production_path['inputs'][key], 4)}").grid(column=0, row=i+1, padx=10, sticky="W")
 
-
 		output_frame = Frame(mainframe)
 		output_frame.grid(column=1, row=4, padx=10, pady=10, sticky="NW")
 		output_frame["borderwidth"] = 2
@@ -342,8 +384,17 @@ class Production_Path_Window:
 		view_path_tree_button = Button(mainframe, text="View Production Tree", command=lambda:Production_Path_Tree_Window(self.root, simple_production_path, production_path))
 		view_path_tree_button.grid(column=1, row=5, sticky="N")
 
+
 class Production_Path_Tree_Window:
 	def __init__(self, root, simple_production_path, production_path):
+		'''
+		Generates a window displaying the structure of a production path's production tree
+
+		Parameters:
+			(tkinter_window) root: master window of this window
+			(dict) simple_production_path: the simplified production path of the provided production path
+			(dict) production_path: the production path to display the values of
+		'''
 		self.root = Toplevel(root)
 		root = self.root
 
@@ -357,22 +408,23 @@ class Production_Path_Tree_Window:
 
 		Label(mainframe, text="Production Path Tree").grid(column=0, row=0)
 
+		#initilize scroll bars
 		horizontal_scroll_bar = Scrollbar(root, orient=HORIZONTAL)
 		vertical_scroll_bar = Scrollbar(root, orient=VERTICAL)
 		horizontal_scroll_bar.grid(column=0, row=1, sticky="EW")
 		vertical_scroll_bar.grid(column=1, row=0, sticky="NS")
 
-		#s = StringVar(value=Setup_Generator.get_production_recipes(production_path))
-		#Listbox(mainframe, height=40, width=80, listvariable=s).grid(column=0, row=1)
-
+		#generate production path tree plan
 		self.ppt_plan = self.get_ppt_plan()
 
+		#set canvas constants for initilization
 		self.recipe_box_width = 200
 		self.recipe_box_height = 75
 		self.recipe_box_padding = (75, 50)
 		self.text_height = 20
 		self.connector_seperation = 10
-
+		
+		#canvas initilization
 		c_width = max(*self.ppt_plan[1]) * (self.recipe_box_width + self.recipe_box_padding[0])
 		c_height = self.recipe_box_padding[1]/2 + (len(self.ppt_plan[1]) - 1) * (self.recipe_box_height + self.recipe_box_padding[1] + (sum(self.ppt_plan[1]) - len(self.ppt_plan[1])) * self.connector_seperation)
 		self.tree_canvas = Canvas(mainframe, background="#232323", yscrollcommand=vertical_scroll_bar.set, xscrollcommand=horizontal_scroll_bar.set, 
@@ -385,17 +437,16 @@ class Production_Path_Tree_Window:
 							)
 		self.tree_canvas.grid(column=0, row=1)
 
+		#link scroll bar commands
 		horizontal_scroll_bar['command'] = self.tree_canvas.xview
 		vertical_scroll_bar['command'] = self.tree_canvas.yview
-
-		#debug print
-		#for row in self.ppt_plan[0]:
-		#	print(row)
 		
+		#generate production tree visualization
 		self._draw_ppt()
 
 
 	def _draw_ppt(self):
+		'''Draws this production path's production tree to this object's canvas'''
 		ppt_rows = self.ppt_plan[0]
 		ppt_dimensions = self.ppt_plan[1]
 
@@ -434,7 +485,7 @@ class Production_Path_Tree_Window:
 					)
 
 				if input_recipe_count == 0: #input resource
-					#draw input line
+					#draw input line. not liked / confusing during user testing
 					'''
 					self.tree_canvas.create_line(
 						line_start[0], line_start[1],
@@ -468,12 +519,12 @@ class Production_Path_Tree_Window:
 					draw_index += input_recipe_count
 
 	def _get_pos_at(self, row, col):
+		'''Returns the canvas position of a specific row and col of recipe (does not accout for vertical offset)'''
 		return (
 				self.recipe_box_padding[0]/2 + (self.recipe_box_width + self.recipe_box_padding[0]) * ((max(*self.ppt_plan[1]) - self.ppt_plan[1][row]) / 2 + col),
 				self.recipe_box_padding[1]/2 + (self.recipe_box_height + self.recipe_box_padding[1]) * row
 			)
 
-	
 	def get_ppt_plan(self, production_path = None, depth = 0):
 		'''
 		Returns this window's production path as a list of rows
@@ -516,6 +567,7 @@ class Production_Path_Tree_Window:
 			return [[list(production_path.keys())[0]]] + production_path_tree, [1] + dimensions #to account for inital recipe of the tree that isn't shown in any inputs
 		else:
 			return production_path_tree, tuple(dimensions)
+
 
 if __name__ == "__main__":
 	Optimal_Satisfaction_UI().mainloop()
